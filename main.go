911a3f1f20ff335066f29ebf3cf08d6a9a726a33
Dave Josephsen <dave@skeptech.org>
2016-05-17 16:14:22 -0500

    working

--- main.go
+++ main.go
@@ -5,49 +5,66 @@ import(
 	"net/http"
 	"net/url"
 	"os"
-   "github.com/Jeffail/gabs"
+   "github.com/Jeffail/gabs" // more easier json parsing
 	"io/ioutil"
 	"strings"
 	)
 
 func helpHandler(w http.ResponseWriter, r *http.Request) {
+// Help screen for those accessing url '/'
    fmt.Fprintf(w, "Welcome to BijBling\nValid targets:\n\n")
    fmt.Fprintf(w, "/alert :: reflects websocket alerts from Librato\n")
 }
 
 func alertHandler(w http.ResponseWriter, r *http.Request) {
+// Handler for url path '/alert', this transforms an alert from librato
+// into an annotation stream back inside librato
 	luser := os.Getenv("BIJ_USER")
    lpass := os.Getenv("BIJ_TOKEN")
 
+	//panic if we don't have librato credentials
+	if luser == `` || lpass == `` {
+		panic("Export BIJ_USER and BIJ_TOKEN with your librato credentials")
+	}
+
+	//parse out the server name and alert name from the alert payload
 	payload,_ := ioutil.ReadAll(r.Body)
 	parsed_payload,_ := gabs.ParseJSON(payload)
    alert, _ := parsed_payload.Path("alert.name").Data().(string)
-	url_path := fmt.Sprintf("http://metrics-api.librato.com/v1/annotations/alerts.%s", alert)
+	url_path := fmt.Sprintf("https://metrics-api.librato.com/v1/annotations/alerts.%s", alert)
 
    violators, _ := parsed_payload.S("violations").ChildrenMap()
 	for host, _ := range violators {
-		form := new(url.Values)
-		form.Add("title", fmt.Sprintf("Alert Fired :: %s ",alert))
-		form.Add("source", host)
-		form.Add("description", fmt.Sprintf("Alert: %s fired on host %s",alert, host))
+		//for each host that violated the threshold, fire off an annotation 
+		//into the annotation stream named for this alert
+		form := url.Values{}
+		form.Set("title", fmt.Sprintf("Alert Fired :: %s ",alert))
+		form.Set("source", host)
+		form.Set("description", fmt.Sprintf("Alert: %s fired on host %s",alert, host))
 		req,_ := http.NewRequest("POST", url_path, strings.NewReader(form.Encode()))
 		defer req.Body.Close()
 		req.SetBasicAuth(luser,lpass)
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-		fmt.Printf("Alert: %s fired on host %s", alert, host)
-
+		fmt.Printf("Alert: %s fired on host %s\n", alert, host)
+		//post the annotation 
 		client := new(http.Client)
 		resp, err := client.Do(req)
+
+		//log the response from librato-api
 		if err != nil {
 			fmt.Printf("ERROR :: %s\n",err)
 		}
-		fmt.Printf("Annotation response: %s", resp.Status)
+		resp_body,_ := ioutil.ReadAll(resp.Body)
+		fmt.Printf("Annotation response: %s\n", resp.Status)
+		fmt.Printf("Annotation response body: %s\n", resp_body)
 	}
 }
 
+//lets kick this pig
 func main() {
+	fmt.Println("Started")
    http.HandleFunc("/", helpHandler)
-   http.HandleFunc("/alert/", alertHandler)
+   http.HandleFunc("/alert", alertHandler)
    http.ListenAndServe(":8080", nil)
 }
